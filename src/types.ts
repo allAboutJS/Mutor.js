60b7f198ae0feb849e06bff85dd50b81b21ddaa9
allAboutJS <victoronah1914@gmail.com>
2026-02-19 13:25:42 +0100

    chore: update types and interfaces

--- src/types.ts
+++ src/types.ts
@@ -1,30 +1,108 @@
-export enum MODE {
+export enum TokenType {
+	// Raw text.
+	TEXT,
+	// Single-character tokens.
+	TERNARY,
+	LEFT_PAREN,
+	RIGHT_PAREN,
+	LEFT_SQUARE_PAREN,
+	RIGHT_SQUARE_PAREN,
+	COMMA,
+	DOT,
+	COLON,
+	MINUS,
+	PLUS,
+	SLASH,
+	STAR,
+	// One or two character tokens.
+	BANG,
+	BANG_EQUAL,
+	EQUAL_EQUAL,
+	GREATER,
+	GREATER_EQUAL,
+	LESS,
+	LESS_EQUAL,
+	// Literals.
+	IDENTIFIER,
+	STRING,
+	NUMBER,
+	// Keywords.
+	AND,
+	ELSE,
+	FALSE,
+	FOR,
+	OF,
+	IF,
+	OR,
+	THIS,
+	END,
+	TRUE,
+	// Template blocks.
+	BLOCK_START,
+	BLOCK_END,
+}
+
+export enum Mode {
 	CODE,
 	TEXT,
 }
 
-export enum TOKEN_TYPE {
-	STRING_LITERAL,
-	PARENTHESES,
-	KEYWORD,
-	TEXT,
-	IDENTIFIER,
-	AT_IDENTIFIER,
-	DOT,
-	COLON,
-	COMMA,
-	NUMERIC_LITERAL,
-	WHITESPACE,
-	OPERATOR,
-	CODE_START,
-	CODE_END,
+export interface Token {
+	type: TokenType;
+	text?: string;
+	line: number;
 }
 
-export enum NODE_TYPE {
+export enum NodeType {
+	TERNARY_EXPRESSION,
 	BINARY_EXPRESSION,
-	IDENTIFIER,
-	MEMBER_EXPRESSION,
-	NUMERIC_LITERAL,
+	UNARY_EXPRESSION,
 	STRING_LITERAL,
+	NUMBER_LITERAL,
+	IDENTIFIER,
+	OBJECT,
+	BLOCK_END,
+	BLOCK_START,
 	TEXT,
+	FOR,
+	END,
+	IF,
+	FUNCTION,
+	GROUPED_EXPRESSION,
 }
+
+export type Expression =
+	| PrimaryExpression
+	| BinaryExpression
+	| TernaryExpression
+	| MemberExpression;
+
+export type PrimaryExpression =
+	| { type: NodeType }
+	| { type: NodeType; value: string | number }
+	| { type: NodeType; name: string; callable?: boolean; args?: Expression[] };
+
+export type BinaryExpression = {
+	type: NodeType;
+	operator: Token;
+	left: Expression;
+	right: Expression;
+};
+
+export type TernaryExpression = BinaryExpression & {
+	condition: Expression;
+};
+
+export type MemberExpression = {
+	type: NodeType;
+	property: Expression;
+	shouldCompute: boolean;
+	object: Expression;
+};
+
+export type IfConditionalExpression = {
+	type: NodeType;
+	body: Expression[];
+	condition: Expression;
+	elseBlock: Expression[] | IfConditionalExpression;
+};
