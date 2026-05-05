a658fc6dfffae58065d9ab41daccf6ba3478d821
Victor Onah <victoronah1914@gmail.com>
2026-05-05 22:30:58 +0100

    chore: fix bug with browser import

--- tsup.config.ts
+++ tsup.config.ts
@@ -12,16 +12,16 @@ export default defineConfig([
     target: "es2020",
   },
   {
-    entry: ["src/browser.ts"],
-    outDir: "dist/browser",
+    entry: {
+      browser: "src/browser.ts",
+    },
+    outDir: "dist",
     format: ["esm", "cjs"],
     dts: true,
     sourcemap: true,
-    clean: true,
+    clean: false,
     minify: true,
     target: "es2020",
-    esbuildOptions(options) {
-      options.platform = "browser";
-    },
+    platform: "browser",
   },
 ]);
